b2703629c934a157ef41b5670a151c23eae6d8d7
= <dario.mennillo@immaginazioneelavoro.it>
2026-06-23 17:58:07 +0200

    up

--- Lezioni_Javascript/Lezione2_Fondamenti/js/script03.js
+++ Lezioni_Javascript/Lezione2_Fondamenti/js/script03.js
@@ -30,9 +30,31 @@ let elencoStudenti = document.getElementById("elencoStudenti");
 // }
 
 //In questo caso, i è l'indice che scorrerà l'array. Questo indic parte da 0 e raggiunge la studenti.length - 1
-for(let i = 0; i < studenti.length; i++){
+for(let i = 0; i < numeroStudenti; i++){
     elencoStudenti.innerHTML += `<li> ${studenti[i]} - ${voti[i]} </li>`
 }
 
 ///PROVO A FARE LA STESSA COSA MA CON 2 DIV SEPARATI: uno per i nomi, uno per i voti
 
+let elNomiStudenti = document.getElementById("elNomiStudenti");
+let elVotiStudenti = document.getElementById("elVotiStudenti");
+
+for(let studente of studenti){
+    elNomiStudenti.innerHTML += `<li> ${studente} </li>`;
+}
+
+for(let voto of voti){
+    elVotiStudenti.innerHTML += `<li> ${voto} </li>`;
+}
+
+let elMediaVoti = document.getElementById("elMediaVoti");
+
+let sommaVoti = 0;
+
+for( let i = 0; i < voti.length; i++){
+    sommaVoti += voti[i];
+}
+
+let mediaVoti = sommaVoti / voti.length ;
+
+elMediaVoti.innerHTML = `Media: ${mediaVoti.toFixed(1)}`;
